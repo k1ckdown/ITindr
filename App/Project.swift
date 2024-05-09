cd7b0e780350242d4d544323840f5bfef83fdfa9
Ivan Semenov <devsemtsk@gmail.com>
2024-05-09 13:27:55 +0700

    Init

---
+++ App/Project.swift
@@ -0,0 +1,28 @@
+import ProjectDescription
+
+let project = Project(
+    name: "ITindr",
+    settings: .settings(
+        base: [
+            "INFOPLIST_KEY_CFBundleDisplayName": .string("ITindr"),
+            "INFOPLIST_KEY_LSApplicationCategoryType": .string("public.app-category.social-networking")
+        ]
+    ),
+    targets: [
+        .target(
+            name: "ITindr",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.ITindr",
+            deploymentTargets: .iOS("15.0"),
+            infoPlist: .extendingDefault(with: [
+                "UISupportedInterfaceOrientations": .array([
+                    .string("UIInterfaceOrientationPortrait")
+                ])
+            ]),
+            sources: ["Sources/**"],
+            resources: ["Resources/**"],
+            dependencies: []
+        )
+    ]
+)
---
+++ App/Sources/ITindrApp.swift
@@ -0,0 +1,18 @@
+//
+//  ITindrApp.swift
+//  ITindr
+//
+//  Created by Ivan Semenov on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ITindrApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            Text("Tindr")
+        }
+    }
+}
