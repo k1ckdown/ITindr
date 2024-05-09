74a896bd24970dda30dc6174b7dd4e7be68c1bd7
Ivan Semenov <devsemtsk@gmail.com>
2024-05-09 19:04:43 +0700

    Add ProjectDescriptionHelpers

---
+++ Tuist/ProjectDescriptionHelpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Ivan Semenov on 09.05.2024.
+//
+
+import ProjectDescription
+
+enum Constants {
+    static let iOSVersion = "15.0"
+    static let bundleId = "com.ITindr"
+    static let sources: SourceFilesList = "Sources/**"
+    static let resources: ResourceFileElements = "Resources/**"
+}
---
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -0,0 +1,62 @@
+//
+//  Project+Templates.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Ivan Semenov on 09.05.2024.
+//
+
+import ProjectDescription
+
+extension Project {
+
+    public static func coreFramework(name: String, dependencies: [TargetDependency] = [], includeResources: Bool = true) -> Project {
+        Project(
+            name: name,
+            targets: [
+                .target(
+                    name: name,
+                    destinations: .iOS,
+                    product: .staticFramework,
+                    bundleId: "\(Constants.bundleId).\(name)",
+                    deploymentTargets: .iOS(Constants.iOSVersion),
+                    infoPlist: .default,
+                    sources: Constants.sources,
+                    resources: includeResources ? Constants.resources : nil,
+                    dependencies: dependencies
+                )
+            ]
+        )
+    }
+
+    public static func featureFramework(name: String, dependencies: [TargetDependency] = []) -> Project {
+        let interfaceName = "\(name)Interface"
+
+        return Project(
+            name: name,
+            targets: [
+                .target(
+                    name: interfaceName,
+                    destinations: .iOS,
+                    product: .staticFramework,
+                    bundleId: "\(Constants.bundleId).\(interfaceName)",
+                    deploymentTargets: .iOS(Constants.iOSVersion),
+                    infoPlist: .default,
+                    sources: "Interface/**"
+                ),
+                .target(
+                    name: name,
+                    destinations: .iOS,
+                    product: .staticFramework,
+                    bundleId: "\(Constants.bundleId).\(name)",
+                    deploymentTargets: .iOS(Constants.iOSVersion),
+                    infoPlist: .default,
+                    sources: Constants.sources,
+                    resources: Constants.resources,
+                    dependencies: dependencies + [
+                        .project(target: interfaceName, path: "../\(name)")
+                    ]
+                )
+            ]
+        )
+    }
+}
